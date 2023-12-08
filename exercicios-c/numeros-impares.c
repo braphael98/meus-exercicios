e6cd27789691e9711953e84fdef580b2425fd381
bruno <braphaelperez98@gmail.com>
2023-12-08 15:10:39 -0300

    Exercicio compreendido

--- exercicios-c/numeros-impares.c
+++ exercicios-c/numeros-impares.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
- int numero, impares = 0, resto;
-    for (int i = 1; i<=10; i++){
-        printf("Digite o numero %i: ",i);
-        scanf("%i",&numero);
-        resto = numero%2;
-        if (resto >0) {  //todo numero impar é maior que 0 no resto da divisão
-            impares++;
+ int num,contagem = 0;
+ for(int i=1;i<11; i++){
+     printf("\n Digite um numero: ");
+     scanf("%i",&num);
+     contagem++;
+ }
+  if(num %2){
+        printf("você tem %i numeros impares",contagem);
         }
-        
-    }
-    printf("Total de impares: %i ",impares);
+
     return 0;
 }
+
